ae07e258f31806ea783aac72fb901a693b927bbf
Preetom Gogoi <preetomgogoi@Preetoms-MacBook-Air.local>
2025-12-07 18:12:14 +0530

    add country code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -98,7 +98,7 @@ Thank you 🙏
         const encodedMessage = encodeURIComponent(message);
 
         // ⭐ Your WhatsApp Number (Change this)
-        const phoneNumber = "8011127009";
+        const phoneNumber = "+918011127009";
 
         const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
